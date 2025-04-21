c680314c3643d6d597b5687d4190f31b56a35465
Yuriy Semenov <ysemenov@weborama.com.ru>
2025-04-21 11:35:34 +0300

    fix gitignore3
    fix gitignore3

--- main.go
+++ main.go
@@ -2,11 +2,40 @@ package main
 
 import "fmt"
 
-func main() {
-	transactions := [5]int{1, 2, 3, 4, 5}
-	transactionsPartial := transactions[1:4]
-	transactionsPartial[0] = 30
+// В цикле спрашиваем у пользователя ввод транзакций: -10, 10, 40.5
+// Добавлять каждую в массив транзакций
+// Выводить массив транзакций
+// !Вывести сумму баланса в консоль
 
-	fmt.Println(transactions)
-	fmt.Println(transactionsPartial)
+func main() {
+	transactions := []float64{} // массив транзакций
+	for {
+		transaction := scanTransaction()
+		if transaction == 0 {
+			break
+		}
+		transactions = append(transactions, transaction)
+	}
+	printTransactions(transactions)
+}
+
+func scanTransaction() float64 {
+	var transaction float64
+	fmt.Print("Введите транзакцию (n - выход): ")
+	fmt.Scanln(&transaction)
+	return transaction
+
+}
+
+func calculateBalance(transactions []float64) float64 {
+	balance := 0.0
+	for _, value := range transactions {
+		balance += value
+	}
+	return balance
+}
+
+func printTransactions(transactions []float64) {
+	balance := calculateBalance(transactions)
+	fmt.Printf("Сумма баланса: %.2f\n", balance)
 }
